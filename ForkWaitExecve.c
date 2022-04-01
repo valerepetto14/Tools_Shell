447b49ab6ef62dae8028cae5e61916f946a57c55
valerepetto14 <valerepetto12345@gmail.com>
2022-04-01 20:08:21 +0000

    better version

--- ForkWaitExecve.c
+++ ForkWaitExecve.c
@@ -11,18 +11,20 @@ int main(void)
 	pid_t pid;
 	int iter = 0;
 	char *argv[] = {"/bin/ls", "-l", "./", NULL};
+
 	while (iter < 5)
 	{
 		pid = fork();
+
 		if (pid == 0)
 		{
 			printf("Before execve\n");
 			if (execve(argv[0], argv, NULL) == -1)
 			{
 				perror("Error:");
-				return(1);
+				return (1);
 			}
-			printf("After execve\n");	
+			printf("After execve\n");
 		}
 		else if (pid < 0)
 		{
--- shell.c
+++ shell.c
@@ -1,29 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 /**
  * main - leer una linea y printearla
  * Return: num
  **/
 int main(void)
 {
+	pid_t hijo;
 	ssize_t bytes_leidos;
-	size_t numero_bytes;
-	char *cadena;
+	size_t numero_bytes = 0;
+	int iter = 0;
+	char *cadena = NULL;
+	char *token;
+	char *argv[4] = {NULL, NULL, NULL, NULL};
 
-	printf("$");
-	numero_bytes = 0;
-	cadena = NULL;
-	bytes_leidos = getline(&cadena, &numero_bytes, stdin);
+	while (1)
+	{
+		iter = 0;
+		printf("#cisfun$ ");
+		bytes_leidos = getline(&cadena, &numero_bytes, stdin);
+		if (bytes_leidos == -1)
+		{
+			continue;
+		}
+		else
+		{
+			cadena = strtok(cadena, "\n");
+			token = strtok(cadena, " ");
+			while (token != NULL && iter < 3) //LLENO EL ARRAY
+			{
+				argv[iter] = token;
+				printf("%s\n", token);
+				iter++;
+				token = strtok(NULL, " ");
+			}
 
-	if (bytes_leidos == -1)
-	{
-		printf("Error.\n");
-	}
-	else
-	{
-		execve(cadena[])
+			hijo = fork();
+			if (hijo == 0) //DENTRO DEL HIJO
+			{
+				if (execve(argv[0], argv, NULL) == -1) //TRATAMOS DE EJECUTA EL COMANDO
+				{
+					perror(" ERROR");
+				}
+			}
+			else //ESTOY EN EL PADRE
+			{
+				wait(NULL);
+				continue;
+			}
+		}
+		free(cadena);
+		bytes_leidos = 0;
+		cadena = NULL;
 	}
 	free(cadena);
-
 	return (0);
 }
