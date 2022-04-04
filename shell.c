c0d736a3bc24f16121d0c147bcd4739462f5607b
valerepetto14 <valerepetto12345@gmail.com>
2022-04-04 16:48:20 +0000

    latest updates

--- shell.c
+++ shell.c
@@ -7,65 +7,6 @@
  * main - leer una linea y printearla
  * Return: num
  **/
-int ContSpaces(char * cadena1);
-int;
-
-int main(void)
-{
-	pid_t hijo;
-	ssize_t bytes_leidos;
-	size_t numero_bytes = 0;
-	int iter = 0, spaces = 0;
-	char *cadena = NULL;
-	char *token;
-	char *argv[] = {NULL};
-
-	while (1)
-	{
-		iter = 0;
-		printf("#cisfun$ ");
-		bytes_leidos = getline(&cadena, &numero_bytes, stdin);
-		if (bytes_leidos == -1 || cadena[0] == '\n')
-		{
-			continue;
-		}
-		else
-		{
-			cadena = strtok(cadena, "\n");
-			spaces = ContSpaces(cadena);
-			argv[] = calloc(spaces, sizeof(char *));
-			token = strtok(cadena, " ");
-			while (token != NULL && iter < spaces) //LLENO EL ARRAY
-			{
-				argv[iter] = token;
-				iter++;
-				token = strtok(NULL, " ");
-			}
-
-			hijo = fork();
-			if (hijo == 0) //DENTRO DEL HIJO
-			{
-				if (execve(argv[0], argv, NULL) == -1) //TRATAMOS DE EJECUTA EL COMANDO
-				{
-					perror(" ERROR");
-				}
-			}
-			else //ESTOY EN EL PADRE
-			{
-				argv[0] = NULL, argv[1] = NULL,argv[2] = NULL, argv[3] = NULL;
-				wait(NULL);
-				continue;
-			}
-		}
-		free(cadena);
-		bytes_leidos = 0;
-		cadena = NULL;
-		argv[0] = NULL, argv[1] = NULL,argv[2] = NULL, argv[3] = NULL;
-	}
-	free(cadena);
-	return (0);
-}
-
 int ContSpaces(char * cadena1)
 {
 	int cont = 0;
@@ -78,3 +19,71 @@ int ContSpaces(char * cadena1)
 	}
 	return (cont);
 }
+
+int main(void)
+{
+	pid_t hijo;
+	ssize_t bytes_leidos;
+	size_t numero_bytes = 0;
+	int iter = 0, spaces = 0;
+	char *copycadena;
+	char *cadena = NULL;
+	char *token;
+	char **argv;
+
+	while (1)
+	{
+		iter = 0;
+		printf("#cisfun$ ");
+		bytes_leidos = getline(&cadena, &numero_bytes, stdin);
+		if (bytes_leidos == -1 || cadena[0] == '\n')
+		{
+			free(cadena);
+			free(copycadena);
+			continue;
+		}
+		else
+		{
+			copycadena = strdup(cadena);
+			cadena = strtok(cadena, "\n");
+			spaces = ContSpaces(copycadena);
+			argv = malloc(sizeof(char *) * spaces);
+			token = strtok(cadena, " ");
+			while (token != NULL && iter <= spaces) //LLENO EL ARRAY
+			{
+				argv[iter] = token;
+				iter++;
+				token = strtok(NULL, " ");
+			}
+
+			hijo = fork();
+			if (hijo == 0) //DENTRO DEL HIJO
+			{
+				if (execve(argv[0], argv, NULL) == -1) //TRATAMOS DE EJECUTA EL COMANDO
+				{
+					perror(" ERROR");
+					free(argv);
+					free(cadena);
+				}
+			}
+			else //ESTOY EN EL PADRE
+			{
+				wait(NULL);
+				free(copycadena);
+				free(cadena);
+				free(argv);
+				continue;
+			}
+		}
+		free(cadena);
+		free(argv);
+		free(copycadena);
+		bytes_leidos = 0;
+		cadena = NULL;
+	}
+	free(argv);
+	free(cadena);
+	return (0);
+}
+
+
