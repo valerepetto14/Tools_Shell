3802bf2e07261989e945c620196194b73f6f8434
valerepetto14 <valerepetto12345@gmail.com>
2022-04-05 16:35:19 +0000

    shell updated

--- shell.c
+++ shell.c
@@ -7,18 +7,6 @@
 #define BLANCO_T   "\x1b[37m"
 #define VERDE_T        "\x1b[32m"
 
-void variable_controlc(int boton)
-{
-boton = 1;
-}
-
-void capturar(int boton)
-{
-struct sigaction act;
-act.sa_handler = variable_controlc(boton);
-sigaction(SIGINT, &act, NULL);
-}
-
 /**
  *main - leer una linea y printearla
  *Return: num
@@ -36,46 +24,40 @@ int ContTokens(char *cadena1)
 	}
 	return (cont);
 }
-
 /**
  *main - simple shell
  *Return: Always 0
  **/
 int main(void)
 {
-	int boton = 0;
 	pid_t hijo;
 	ssize_t bytes_leidos;
 	size_t numero_bytes = 0;
 	int iter = 0, tokens = 0;
-	char *copycadena;
-	char *cadena = NULL;
-	char *token;
-	char **argv;
-
+	char *copycadena, *token, **argv, *cadena = NULL, *salir = "exit";
 	while (1)
 	{
-		capturar(boton);
 		iter = 0;
 		printf(VERDE_T "$cisfun " BLANCO_T);
 		bytes_leidos = getline(&cadena, &numero_bytes, stdin);
-		if (bytes_leidos == -1 || cadena[0] == '\n')
+		if (cadena[0] == '\n')
 		{
+			cadena = NULL;
 			continue;
 		}
+		else if (bytes_leidos == -1 || cadena[0] == EOF)
+			break;
 		else
 		{
-			if(cadena[0] == 'E'&& cadena[1] == 'x' && cadena[2] == 'i' && cadena[3] == 't')
-			{
-				break;
-			}
-			else if (boton == 1)
-				continue;
 			copycadena = strdup(cadena);
 			cadena = strtok(cadena, "\n");
 			tokens = ContTokens(copycadena);
-			argv = calloc(tokens + 1, sizeof(char*));
+			argv = calloc(tokens + 1, sizeof(char *));
 			token = strtok(cadena, " ");
+
+			if (strcmp(token, salir) == 0)
+				break;
+
 			while (token != NULL && iter < tokens) //LLENO EL ARRAY
 			{
 				argv[iter] = token;
@@ -85,6 +67,7 @@ int main(void)
 			argv[tokens] = NULL;
 
 			hijo = fork();
+
 			if (hijo == 0) //DENTRO DEL HIJO
 			{
 				if (execve(argv[0], argv, NULL) == -1) //TRATAMOS DE EJECUTA EL COMANDO
